fc0cebc3e6fa8d43e1825c11a2164587770b90a9
Flavien <flavhirtz@gmail.com>
2023-01-13 18:22:20 +0100

    adding interface for Login props

--- app-client/src/components/Login.tsx
+++ app-client/src/components/Login.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Login.scss";
 
-function Login({onLogin}: { onLogin: () => void }) {
+interface LoginProps {
+  onLogin: () => void;
+}
+
+function Login({onLogin}: LoginProps) {
   const usernameRef = React.useRef<HTMLInputElement>(null);
 
   const login = (e: React.FormEvent) => {
@@ -18,14 +22,14 @@ function Login({onLogin}: { onLogin: () => void }) {
         <form action="" onSubmit={login}>
           <h1>WELCOME !</h1>
           <div className={"container"}>
-          <input id="username" type="text" placeholder={" "} ref={usernameRef} autoFocus={true} autoComplete={"off"}/>
-          <label htmlFor="username" className={"movingLabel"}>Username</label>
-        </div>
-        <div className={"container"}>
-          <input id="password" type="password" placeholder={" "}/>
-          <label htmlFor="password" className={"movingLabel"}>Password</label>
-        </div>
-        <button type="submit">Login</button>
+            <input id="username" type="text" placeholder={" "} ref={usernameRef} autoFocus={true} autoComplete={"off"}/>
+            <label htmlFor="username" className={"movingLabel"}>Username</label>
+          </div>
+          <div className={"container"}>
+            <input id="password" type="password" placeholder={" "}/>
+            <label htmlFor="password" className={"movingLabel"}>Password</label>
+          </div>
+          <button type="submit">Login</button>
         </form>
       </div>
   )
