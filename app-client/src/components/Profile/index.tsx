38711fbae105d7b014dee6520a423185ca688fcb
Flavien <flavhirtz@gmail.com>
2023-01-25 18:04:30 +0100

    removing align-items center in root

--- app-client/src/components/Profile/index.tsx
+++ app-client/src/components/Profile/index.tsx
@@ -11,11 +11,13 @@ interface HomeProps {
 function Profile({ tryLogout }: HomeProps) {
   return (
     <StrictMode>
+        <div className="flex flex-col h-screen w-screen justify-between">
       <Navbar tryLogout={tryLogout} />
       <main>
         <h1>Profile</h1>
       </main>
       <Footer />
+    </div>
     </StrictMode>
   );
 }
