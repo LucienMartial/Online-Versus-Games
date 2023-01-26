56e0bdcda5353a523d7db4ea37e27fe8487471bc
Flavien <flavhirtz@gmail.com>
2023-01-26 22:16:49 +0100

    adding icons for overview and history

--- app-client/src/components/user/Profile.tsx
+++ app-client/src/components/user/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Overview from "./Overview";
 import History from "./History";
+import { FiUser } from "react-icons/fi";
+import { FaHistory } from "react-icons/all";
 
 interface ProfileProps {
   username: string;
@@ -33,22 +35,24 @@ export default function Profile({ username }: ProfileProps) {
   return (
     <main className="h-full flex flex-col min-h-0">
       <div className=" grid grid-cols-2 text-xl">
-        <h2
-          className={`cursor-pointer p-4 border-b-2 ${
+        <div
+          className={`flex flex-row justify-center items-center cursor-pointer p-4 border-b-2 ${
             currentTab === "overview" ? activeTabStyle : inactiveTabStyle
           }`}
           onClick={() => setCurrentTab("overview")}
         >
-          Overview
-        </h2>
-        <h2
-          className={`cursor-pointer p-4 border-b-2 ${
+          <FiUser className="inline-block mr-2" />
+          <h2>Overview</h2>
+        </div>
+        <div
+          className={`flex flex-row justify-center items-center cursor-pointer p-4 border-b-2 ${
             currentTab === "history" ? activeTabStyle : inactiveTabStyle
           }`}
           onClick={() => setCurrentTab("history")}
         >
-          History
-        </h2>
+          <FaHistory className="inline-block mr-2" />
+          <h2>History</h2>
+        </div>
       </div>
       <section className="min-h-0 flex flex-col">
         <h1 className={"my-4"}>{renderTitle()}</h1>
