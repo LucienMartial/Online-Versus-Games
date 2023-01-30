06fd4044c9750b415c8e32ad4b07331410db32e4
Flavien <flavhirtz@gmail.com>
2023-01-30 19:05:15 +0100

    adding Tabs component to easily use tabs with title, (logo) and content

---
+++ app-client/src/components/lib/Tabs.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useState } from "react";
+
+interface TabData {
+  title: string;
+  logo?: ReactNode;
+  content: ReactNode;
+}
+
+interface TabsProps {
+  tabsDatas: TabData[];
+}
+
+const activeTabStyle = "border-blue-400 text-blue-400";
+const inactiveTabStyle = "border-blue-900";
+
+export default function Tabs({ tabsDatas }: TabsProps) {
+  const [currentTab, setCurrentTab] = useState(tabsDatas[0].title);
+
+  return (
+    <main className="h-full flex flex-col min-h-0">
+      <>
+        <div className={`grid grid-cols-${tabsDatas.length} text-lg`}>
+          {tabsDatas.map((data) => (
+            <div
+              className={`flex flex-row justify-center items-center cursor-pointer p-3 border-b-2 ${
+                currentTab === data.title ? activeTabStyle : inactiveTabStyle
+              }`}
+              onClick={() => setCurrentTab(data.title)}
+            >
+              {data.logo && (
+                <div className="inline-block mr-2">{data.logo}</div>
+              )}
+              <h2>{data.title}</h2>
+            </div>
+          ))}
+        </div>
+        {
+          tabsDatas[
+            tabsDatas.findIndex((header) => header.title === currentTab)
+          ].content
+        }
+      </>
+    </main>
+  );
+}
--- app-client/src/components/user/Profile.tsx
+++ app-client/src/components/user/Profile.tsx
@@ -4,35 +4,18 @@ import History from "./History";
 import { FiUser } from "react-icons/fi";
 import { FaHistory } from "react-icons/all";
 import LoadingPage from "../LoadingPage";
+import Tabs from "../lib/Tabs";
 
 interface ProfileProps {
   username: string;
   handleRemoveAccount?: () => Promise<void>;
 }
 
-const activeTabStyle = "border-blue-400 text-blue-400";
-const inactiveTabStyle = "border-blue-900";
-
 export default function Profile({
   username,
   handleRemoveAccount,
 }: ProfileProps) {
-  const [currentTab, setCurrentTab] = useState("overview");
   const [exist, setExist] = useState<boolean | null>(null);
-
-  function renderTabs() {
-    switch (currentTab) {
-      case "overview":
-        return (
-          <Overview
-            username={username}
-            handleRemoveAccount={handleRemoveAccount}
-          />
-        );
-      case "history":
-        return <History username={username} />;
-    }
-  }
 
   useEffect(() => {
     const load = async () => {
@@ -48,41 +31,27 @@ export default function Profile({
     load();
   }, [username]);
 
+  if (exist === null) return <LoadingPage />;
+  if (!exist) return <h2 className="text-2xl">This user does not exist...</h2>;
   return (
-    <main className="h-full flex flex-col min-h-0 grow">
-      <div className="grid grid-cols-2 text-lg">
-        <div
-          className={`flex flex-row justify-center items-center cursor-pointer p-3 border-b-2 ${
-            currentTab === "overview" ? activeTabStyle : inactiveTabStyle
-          }`}
-          onClick={() => setCurrentTab("overview")}
-        >
-          <FiUser className="inline-block mr-2" />
-          <h2>Overview</h2>
-        </div>
-        <div
-          className={`flex flex-row justify-center items-center cursor-pointer p-3 border-b-2 ${
-            currentTab === "history" ? activeTabStyle : inactiveTabStyle
-          }`}
-          onClick={() => setCurrentTab("history")}
-        >
-          <FaHistory className="inline-block mr-2" />
-          <h2>History</h2>
-        </div>
-      </div>
-      {exist === null && (
-        <main className="h-full flex flex-col min-h-0 grow justify-center items-center">
-          <LoadingPage />
-        </main>
-      )}
-      {exist && (
-        <section className="min-h-0 grow flex flex-col">{renderTabs()}</section>
-      )}
-      {exist === false && (
-        <section className="min-h-0 grow flex flex-col items-center justify-center">
-          <h2 className="text-2xl">This user does not exist..</h2>
-        </section>
-      )}
-    </main>
+    <Tabs
+      tabsDatas={[
+        {
+          title: "Overview",
+          logo: <FiUser />,
+          content: (
+            <Overview
+              username={username}
+              handleRemoveAccount={handleRemoveAccount}
+            />
+          ),
+        },
+        {
+          title: "History",
+          logo: <FaHistory />,
+          content: <History username={username} />,
+        },
+      ]}
+    />
   );
 }
