0ebad0d0fbcad8e34f5508466280a85c572916ec
Riwan <coefficriwan@gmail.com>
2023-02-13 15:41:05 +0100

    main ui story

--- app-client/src/stories/MainUI.stories.tsx
+++ app-client/src/stories/MainUI.stories.tsx
@@ -1,10 +1,10 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import Navbar from "../components/lib/Navbar";
+import MainUI from "../components/lib/MainUI";
 import { MemoryRouter } from "react-router-dom";
 
 export default {
-  title: "lib/Navbar",
-  component: Navbar,
+  title: "lib/MainUI",
+  component: MainUI,
   decorators: [
     (Story) => (
       <MemoryRouter initialEntries={["/"]}>
@@ -15,9 +15,9 @@ export default {
   parameters: {
     layout: "fullscreen",
   },
-} as ComponentMeta<typeof Navbar>;
+} as ComponentMeta<typeof MainUI>;
 
-const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
+const Template: ComponentStory<typeof MainUI> = (args) => <MainUI {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
