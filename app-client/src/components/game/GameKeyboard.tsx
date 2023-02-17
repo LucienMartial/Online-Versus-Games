cf60343dca16a99116bdda98261f3d619dd568df
Flavien <flavhirtz@gmail.com>
2023-02-17 23:02:32 +0100

    gamekeyboard style

--- app-client/src/components/game/GameKeyboard.tsx
+++ app-client/src/components/game/GameKeyboard.tsx
@@ -18,21 +18,21 @@ export default function GameKeyboard({
   shieldButton,
 }: GameKeyboardProps) {
   return (
-    <div className="flex w-screen justify-evenly absolute bottom-0 left-0 z-10">
-      <div className="flex aspect-[2/1]">
-        <div className="flex flex-row items-center">
-          <button ref={leftButton}>LEFT</button>
-        </div>
-        <div className="flex flex-col justify-between">
-          <button ref={upButton}>UP</button>
-          <button ref={downButton}>DOWN</button>
-        </div>
-        <div className="flex flex-row items-center">
-          <button ref={rightButton}>RIGHT</button>
-        </div>
+    <div className="flex w-screen justify-evenly items-center absolute bottom-10 left-0 z-10 text-slate-900">
+      <div className="grid grid-cols-3 grid-rows-3">
+        <span></span>
+        <button className="bg-slate-300 aspect-[1/1] rounded-tl-2xl rounded-tr-2xl w-[10vw]" ref={upButton}></button>
+        <span></span>
+        <button className="bg-slate-300 aspect-[1/1] rounded-tl-2xl rounded-bl-2xl" ref={leftButton}></button>
+        <span></span>
+        <button className="bg-slate-300 aspect-[1/1] rounded-tr-2xl rounded-br-2xl" ref={rightButton}></button>
+        <span></span>
+        <button className="bg-slate-300 aspect-[1/1] rounded-bl-2xl rounded-br-2xl" ref={downButton}></button>
+        <span></span>
+
       </div>
-      <button ref={dashButton}>DASH</button>
-      <button ref={shieldButton}>SHIELD</button>
+      <button className="bg-slate-300 rounded h-fit px-5 py-2.5" ref={dashButton}>DASH</button>
+      <button className="bg-slate-300 rounded h-fit px-5 py-2.5" ref={shieldButton}>SHIELD</button>
     </div>
   );
 }
