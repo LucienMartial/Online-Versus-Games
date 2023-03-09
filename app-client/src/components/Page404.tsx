26696f1983589bfbdec3d78782377354843c7bd4
Lucien <lucienmartial33@gmail.com>
2023-03-09 14:21:12 +0100

    404 page updated

--- app-client/src/components/Page404.tsx
+++ app-client/src/components/Page404.tsx
@@ -1,12 +1,66 @@
 import { Link } from "react-router-dom";
 
+function randomImage() {
+  const number = Math.floor(Math.random() * 3) + 1;
+
+  console.log(number);
+
+  switch (number) {
+    case 1:
+      return (
+        <div
+          className={
+            "grow w-full bg-contain bg-no-repeat bg-center bg-[url('/assets/static-pages/monster.svg')]"
+          }
+        ></div>
+      );
+    case 2:
+      return (
+        <div
+          className={
+            "grow w-full bg-contain bg-no-repeat bg-center bg-[url('/assets/static-pages/space.svg')]"
+          }
+        ></div>
+      );
+    case 3:
+      return (
+        <div
+          className={
+            "grow w-full bg-contain bg-no-repeat bg-center bg-[url('/assets/static-pages/robot.svg')]"
+          }
+        ></div>
+      );
+    default:
+      return (
+        <div
+          className={
+            "grow w-full bg-contain bg-no-repeat bg-center bg-[url('/assets/static-pages/monster.svg')]"
+          }
+        ></div>
+      );
+  }
+}
+
 export default function Page404() {
   return (
-    <div className="404">
-      <h1>404</h1>
-      <h2>Page not found</h2>
-      <p>The page you are looking for does not exist.</p>
-      <Link to={"/"}>Go back to home</Link>
+    <div className={"flex flex-col w-full"}>
+      <div className={"mt-8"}>
+        <h1 className={"text-6xl font-bold"}>Unable to find the page</h1>
+        <br />
+        <p className={"text-xl mt-4"}>
+          The page you are looking for does not exist...
+          <br />
+          If you are lost try returning to the{" "}
+          <Link to={"/"} className={"font-bold text-blue-400"}>
+            home page
+          </Link>
+          .
+        </p>
+      </div>
+      {randomImage()}
+      <div className={"flex justify-end text-gray-400 font-black my-4 mx-8"}>
+        <a href="https://storyset.com/web">Web illustrations by Storyset</a>
+      </div>
     </div>
   );
 }
